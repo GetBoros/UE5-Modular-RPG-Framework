abcaff1e30b3cc481a2e89beb19d072bc586c9f4
GetBoros <getboros@gmail.com>
2026-01-06 10:44:08 +0300

    feat: Add simple example with DA_ to plugin content

--- TLGame/Source/TLGame/Public/Data/TLTypes.h
+++ TLGame/Source/TLGame/Public/Data/TLTypes.h
@@ -18,9 +18,9 @@ USTRUCT(BlueprintType) struct FPlayerResponse
     GENERATED_BODY()
 
 public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") EDialogueResponseCategory Category = EDialogueResponseCategory::Aggressive;  // Категория (влияет на визуал кнопки)
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") float SanityCost = 0.0f;  // Стоимость выбора Например: Требует Sanity > 10 Тут можно будет расширить логику позже
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") FText ResponseText;  // Текст на кнопке: "Закрой рот!"
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") EDialogueResponseCategory Category = EDialogueResponseCategory::Aggressive;  // Категория (влияет на визуал кнопки)
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") FGameplayTagContainer ApplyTags;  // Тэги, которые этот ответ добавляет (Эффекты) // Например: State.Enemy.Mood.Broken
 
 };
@@ -30,25 +30,12 @@ USTRUCT(BlueprintType) struct FDialogueNode : public FTableRowBase
     GENERATED_BODY()
 
 public:
-    // Уникальный ID для дебага
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FName RowID;
-
-    // Текст Врага: "Ты опять пялился на ту кассиршу!"
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue", meta = (MultiLine = true))
-    FText NPC_Line;
-
-    // Картинка эмоции (опционально, если хотим хардкод, но лучше через теги)
-    // UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue", meta = (MultiLine = true) ) FText NPC_Line;  // Текст Врага: "Ты опять пялился на ту кассиршу!"
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Conditions") FGameplayTagContainer RequiredConditions;  // Требования для показа этого узла (если используем рандом) Например: Враг должен быть Angry1
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") TArray<FPlayerResponse> PlayerResponses;  // Список ответов Игрока
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue") FName RowID;
+    // Картинка эмоции (опционально, если хотим хардкод, но лучше через теги) UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
     // UTexture2D* PortraitOverride;
-
-    // Требования для показа этого узла (если используем рандом)
-    // Например: Враг должен быть Angry
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Conditions")
-    FGameplayTagContainer RequiredConditions;
-
-    // Список ответов Игрока
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TArray<FPlayerResponse> PlayerResponses;
 };
 //------------------------------------------------------------------------------------------------------------
