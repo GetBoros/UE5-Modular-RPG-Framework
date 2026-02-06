1eaf234472a144abd4b62b31d81daa43e8b705e3
GetBoros <getboros@gmail.com>
2026-02-06 18:51:50 +0300

    feat: Add fatigue accumulator rate to player state

--- TLGame/Source/TLGame/Private/System/TLG_Player_State.cpp
+++ TLGame/Source/TLGame/Private/System/TLG_Player_State.cpp
@@ -43,29 +43,30 @@ UTLG_Attribute_Set *ATLG_Player_State::Get_Attribute_Set() const
 //------------------------------------------------------------------------------------------------------------
 void ATLG_Player_State::Handle_Time_Advanced(int32 hours, int32 minutes, int32 minutes_delta)
 {
-    /* 24h 0.069 (Ciborg) 16h 0.104 (Norm) 12h 0.138 (Weak) 08h 0.208 (Sick) */
-    constexpr float fatigue_accumulation_rate = 0.105f;  // 0.1f 16h 960min 96 Fatigue || 0.2f 8h 480min 96 fatigue
-    const float fatigue_to_apply = static_cast<float>(minutes_delta) * fatigue_accumulation_rate;
-    const FGameplayTag &gameplay_tag_state_fatiigue = FTLG_Data_Gameplay_Tags::Get().State_Fatigued;
-    const FGameplayTag &gameplay_tag_action_system_sleep = FTLG_Data_Gameplay_Tags::Get().Action_System_Sleep;
+    
+    const float fatigue_to_apply = static_cast<float>(minutes_delta) * Fatigue_Accumulation_Rate;
     FGameplayEffectContextHandle gameplay_effect_handle_context;
     FGameplayEffectSpecHandle gameplay_effect_handle_spec;
+    const FGameplayTag &gameplay_tag_state_fatiigue = FTLG_Data_Gameplay_Tags::Get().State_Fatigued;
+    const FGameplayTag &gameplay_tag_action_system_sleep = FTLG_Data_Gameplay_Tags::Get().Action_System_Sleep;
 
+    // 1.0. Init
     if (Gameplay_Effect_Fatigue_Class == 0 || minutes_delta <= 0)
         return;
 
     gameplay_effect_handle_context = Ability_System_Component->MakeEffectContext();
     gameplay_effect_handle_context.AddSourceObject(this);
-
     gameplay_effect_handle_spec= Ability_System_Component->MakeOutgoingSpec(Gameplay_Effect_Fatigue_Class, 1.0f, gameplay_effect_handle_context);
     if (gameplay_effect_handle_spec.IsValid() != true)
         return;
 
+	// 2.0. Apply fatigue, if sleeping reduce fatigue, otherwise increase it
     if (Ability_System_Component->HasMatchingGameplayTag(gameplay_tag_action_system_sleep) == true)
         gameplay_effect_handle_spec.Data.Get()->SetSetByCallerMagnitude(gameplay_tag_state_fatiigue, -fatigue_to_apply);
     else
         gameplay_effect_handle_spec.Data.Get()->SetSetByCallerMagnitude(gameplay_tag_state_fatiigue, fatigue_to_apply);
 
+	// 2.1. Apply the effect to self
     Ability_System_Component->ApplyGameplayEffectSpecToSelf(*gameplay_effect_handle_spec.Data.Get() );
 }
 //------------------------------------------------------------------------------------------------------------
