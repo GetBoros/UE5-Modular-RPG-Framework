c4a39e500dd7e0665cdf0acca7a07e0d7f5feebc
GetBoros <getboros@gmail.com>
2026-02-12 20:28:24 +0300

    feat: Add TLG_Widget_Stat moved logic from BP to CPP

--- TLGame/Source/TLGame/Private/UI/TLG_Widget_Stat.cpp
+++ TLGame/Source/TLGame/Private/UI/TLG_Widget_Stat.cpp
@@ -1,4 +1,40 @@
 //------------------------------------------------------------------------------------------------------------
 #include <UI/TLG_Widget_Stat.h>
+
+#include <Components/Image.h>
+#include <Components/TextBlock.h>
 //------------------------------------------------------------------------------------------------------------
 
+
+
+
+// UTLG_Widget_Stat
+void UTLG_Widget_Stat::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+}
+//------------------------------------------------------------------------------------------------------------
+void UTLG_Widget_Stat::NativePreConstruct()
+{
+    if (ensureMsgf(Tooltip_Class, TEXT("Floating Text Class not setting up")) != true)
+        return;
+    
+    if (Cached_Tooltip == 0)
+        Cached_Tooltip = CreateWidget<UUserWidget>(GetOwningPlayer(), Tooltip_Class);
+    else
+        SetToolTip(Cached_Tooltip);
+
+    Update_Value(0.0f);
+
+    Super::NativePreConstruct();
+}
+//------------------------------------------------------------------------------------------------------------
+void UTLG_Widget_Stat::Update_Value(float new_value)
+{
+    if (Image_Icon == 0  && Icon_Texture == 0)
+        Image_Icon->SetBrushFromTexture(Icon_Texture);
+
+    TB_Description->SetText(FText::AsNumber(new_value) );
+}
+//------------------------------------------------------------------------------------------------------------
--- TLGame/Source/TLGame/Public/UI/TLG_Widget_Stat.h
+++ TLGame/Source/TLGame/Public/UI/TLG_Widget_Stat.h
@@ -5,11 +5,27 @@
 
 #include <TLG_Widget_Stat.generated.h>
 //------------------------------------------------------------------------------------------------------------
+class UImage;
+class UTextBlock;
+//------------------------------------------------------------------------------------------------------------
 UCLASS() class TLGAME_API UTLG_Widget_Stat : public UUserWidget
 {
 	GENERATED_BODY()
 	
 public:
+	virtual void NativeConstruct();
+	virtual void NativePreConstruct();
+
+	UFUNCTION(BlueprintCallable) void Update_Value(float new_value);
+
+private:
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true") ) TObjectPtr<UTexture2D> Icon_Texture;
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true") ) TSubclassOf<UUserWidget> Tooltip_Class;
+
+	UPROPERTY(meta = (BindWidget) ) TObjectPtr<UImage> Image_Icon;
+	UPROPERTY(meta = (BindWidget) ) TObjectPtr<UTextBlock> TB_Description;
+
+	UPROPERTY() TObjectPtr<UUserWidget> Cached_Tooltip;
 
 };
 //------------------------------------------------------------------------------------------------------------
