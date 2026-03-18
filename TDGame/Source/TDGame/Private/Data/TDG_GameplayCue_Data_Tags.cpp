d09ed0acc1348443444535865c6711992d94dc5d
GetBoros <getboros@gmail.com>
2026-03-18 16:06:41 +0300

    feat(GAS) Add ability with duration

--- TDGame/Source/TDGame/Private/Data/TDG_GameplayCue_Data_Tags.cpp
+++ TDGame/Source/TDGame/Private/Data/TDG_GameplayCue_Data_Tags.cpp
@@ -6,19 +6,19 @@
 
 
 // FTDG_GameplayCue_Data_Tags
-FTDG_GameplayCue_Data_Tags FTDG_GameplayCue_Data_Tags::TDG_Data_Gameplay_Tags;
+FTDG_GameplayCue_Data_Tags FTDG_GameplayCue_Data_Tags::TDG_GameplayCue_Data_Tags;
 //------------------------------------------------------------------------------------------------------------
 const FTDG_GameplayCue_Data_Tags &FTDG_GameplayCue_Data_Tags::Get()
 {
-    return TDG_Data_Gameplay_Tags;
+    return TDG_GameplayCue_Data_Tags;
 }
 //------------------------------------------------------------------------------------------------------------
 void FTDG_GameplayCue_Data_Tags::Initialize_Native_Tags()
 {
     UGameplayTagsManager &gameplay_tags_manager = UGameplayTagsManager::Get();
 
-    TDG_Data_Gameplay_Tags.GameplayCue_Tutorial_Sound = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.Tutorial.Sound") );
-    TDG_Data_Gameplay_Tags.GameplayCue_Tutorial_Static = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.Tutorial.Static"));
-    TDG_Data_Gameplay_Tags.GameplayCue_Tutorial_Niagara = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.Tutorial.Niagara"));
+    TDG_GameplayCue_Data_Tags.GameplayCue_TDG_Tutorial_Sound = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.TDG.Tutorial.Sound") );
+    TDG_GameplayCue_Data_Tags.GameplayCue_TDG_Tutorial_Static = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.TDG.Tutorial.Static") );
+    TDG_GameplayCue_Data_Tags.GameplayCue_TDG_Tutorial_Niagara = gameplay_tags_manager.AddNativeGameplayTag(TEXT("GameplayCue.TDG.Tutorial.Niagara") );
 }
 //------------------------------------------------------------------------------------------------------------
--- TDGame/Source/TDGame/Public/Data/TDG_GameplayCue_Data_Tags.h
+++ TDGame/Source/TDGame/Public/Data/TDG_GameplayCue_Data_Tags.h
@@ -8,14 +8,14 @@ struct FTDG_GameplayCue_Data_Tags
 public:
     static void Initialize_Native_Tags();
     
-    static const FTDG_GameplayCue_Data_Tags&Get();
+    static const FTDG_GameplayCue_Data_Tags &Get();
 
-    FGameplayTag GameplayCue_Tutorial_Sound;
-    FGameplayTag GameplayCue_Tutorial_Static;
-    FGameplayTag GameplayCue_Tutorial_Niagara;
+    FGameplayTag GameplayCue_TDG_Tutorial_Sound;
+    FGameplayTag GameplayCue_TDG_Tutorial_Static;
+    FGameplayTag GameplayCue_TDG_Tutorial_Niagara;
 
 private:
-    static FTDG_GameplayCue_Data_Tags TDG_Data_Gameplay_Tags;
+    static FTDG_GameplayCue_Data_Tags TDG_GameplayCue_Data_Tags;
 
 };
 //------------------------------------------------------------------------------------------------------------
